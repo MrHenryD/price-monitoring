0480681286400875a92d15e1eb062693a8f56e5d
henrydang26 <henrydang26@gmail.com>
2021-10-24 22:55:09 -0400

    add docker build script

---
+++ scripts/build_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BASE=henry/price-monitoring
+VERSION=$1
+
+if [ $# -eq 0 ]; then
+    echo "No version tag provided"
+    exit 1
+fi
+
+echo "Building image $BASE:$VERSION..."
+docker build -t $BASE:$VERSION .
+echo "Finished building image"
